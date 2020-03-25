efc61e960dc39712c3e9581445eaa15c325731a4
Lee, youngsun <dlsnsiti@gmail.com>
2020-03-26 02:57:07 +0900

    2주차 과제(sum1, sum2)

---
+++ hw1_1/sum1.js
@@ -0,0 +1,6 @@
+let sum = 0;
+
+for(let i = 1; i <= 55; i++)
+    sum += i;
+
+console.log(sum);
---
+++ hw1_1/sum2.js
@@ -0,0 +1,8 @@
+let sum = 0;
+
+for(let i = 1 ; i <= 55 ; i++) {
+    if(i%3 == 0)
+        sum += i;
+}
+
+console.log(sum);
