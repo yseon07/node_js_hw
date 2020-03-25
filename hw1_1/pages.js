1b11366bc20c5ef8988066f57940936cf08e46c1
Lee, youngsun <dlsnsiti@gmail.com>
2020-03-26 03:52:51 +0900

    2주차 과제(pages.js, random.js)

---
+++ hw1_1/pages.js
@@ -0,0 +1,4 @@
+let sc = 30; // student Count
+let ps = 10; // page Size
+
+console.log(sc%ps==0 ? sc/ps : Math.floor(sc/ps+1));
---
+++ hw1_1/random.js
@@ -0,0 +1,4 @@
+for (let i = 0 ; i < 30 ; i++) {
+    let a = Math.floor(Math.random() * 11 + 10);
+    console.log(a);
+}
